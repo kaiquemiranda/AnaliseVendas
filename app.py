8835d336ed532ef929f8bd9886aa785a8556ab53
kaique miranda <kaique.miranda1910@gmail.com>
2024-10-21 12:18:38 -0300

    Alteração layout

--- app.py
+++ app.py
@@ -39,46 +39,51 @@ if uploaded_file is not None:
 
 
             if not df_filtrado.empty:
-                st.dataframe(df_filtrado[['N.º de venda', 'SKU', 'Unidades', 'Data da venda', 'Título do anúncio',
-                                          'Receita por produtos (BRL)', 'frete real',
-                                          'Tarifa de venda e impostos', 'Total (BRL)']])
+
+
+                buffer = BytesIO()
 
                 vendas_por_sku = df_filtrado.groupby('SKU').agg({
                                                                 'SKU': 'first',
                                                                 'Título do anúncio': 'first',  # Mantém o primeiro título do anúncio
                                                                 'Receita por produtos (BRL)': 'sum',  # Soma a receita
-                                                                'Unidades': 'sum'  # Soma as unidades
+                                                                'Unidades': 'sum',  # Soma as unidades
                                                                 }).sort_values(by='Unidades', ascending=False)
 
-                vendas_por_sku = vendas_por_sku.sort_values(by='Unidades', ascending=False)
 
-                st.markdown(f"Total: {df_filtrado['Unidades'].sum()}")
-                buffer = BytesIO()
 
                 with pd.ExcelWriter(buffer, engine='openpyxl') as writer:
                     vendas_por_sku.to_excel(writer, index=False, sheet_name='Vendas por SKU')
 
-                buffer.seek(0)
-
-                st.download_button(
-                    label="Baixar Excel",
-                    data=buffer,
-                    file_name="vendas_por_sku.xlsx",
-                    mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-                )
-
-
-
-                fig = px.bar(vendas_por_sku.head(10),
-                             x='SKU',
-                             y='Unidades',
+                fig = px.bar(vendas_por_sku.head(10).sort_values(by='Unidades', ascending=True),
+                             y='SKU',
+                             x='Unidades',
                              title=f'Produtos mais vendidos para as palavras-chave: {", ".join(palavras_chave).capitalize()}',
                              labels={'SKU': 'SKU', 'Unidades': 'Quantidade de Vendas'},
                              text_auto=True,
                              hover_data={'Título do anúncio': True})
 
-                st.plotly_chart(fig, use_container_width=True)
 
+                with st.container():
+                    col1, col2 = st.columns([1, 1])
+                    with col1:
+                        st.dataframe(
+                            df_filtrado[['N.º de venda', 'SKU', 'Unidades', 'Data da venda', 'Título do anúncio',
+                                         'Receita por produtos (BRL)', 'frete real',
+                                         'Tarifa de venda e impostos', 'Total (BRL)', 'Loja oficial']])
+
+                        st.markdown(f"Total: {df_filtrado['Unidades'].sum()}")
+
+                        buffer.seek(0)
+
+                        st.download_button(
+                            label="Baixar Excel",
+                            data=buffer,
+                            file_name="vendas_por_sku.xlsx",
+                            mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+                        )
+                    with col2:
+                        st.plotly_chart(fig, use_container_width=True)
 
 
             else:
